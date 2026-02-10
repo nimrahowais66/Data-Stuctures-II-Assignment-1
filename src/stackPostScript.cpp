e4bae5bf16e64fe134e6d26916ea71e2020d2a40
Nimrah Mustafa <nimrahowais66@gmail.com>
2026-02-10 15:37:41 +0500

    Stack Functions

--- src/stackPostScript.cpp
+++ src/stackPostScript.cpp
@@ -19,3 +19,23 @@ void StackPostScript :: reverseStack(){
 }
 
 
+void StackPostScript :: push(long double item){
+    vctrstack.push_back(item);
+}
+
+long double StackPostScript :: pop(){
+    if (is_empty()) {
+        throw std::runtime_error("Stack underflow: Attempted to pop from an empty stack."); 
+    }
+    long double popped = vctrstack.back();
+    vctrstack.pop_back();
+    return popped;
+}
+
+long double StackPostScript :: peek(){
+    if (is_empty()) {
+        throw std::runtime_error("Stack underflow: Attempted to peek from an empty stack."); 
+    }
+    return vctrstack.back();
+}
+
