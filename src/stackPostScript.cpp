4275e4be6010d93d0f1958b323883c163fd95bf8
Nimrah Mustafa <nimrahowais66@gmail.com>
2026-02-10 14:55:30 +0500

    stack functions

--- src/stackPostScript.cpp
+++ src/stackPostScript.cpp
@@ -1,5 +1,21 @@
 #include "../include/stackPostScript.hpp"
 
-/*
-Write the implementation for the appropriate methods here
-*/
+StackPostScript :: StackPostScript(){
+    vctrstack;
+}
+
+void StackPostScript :: reverseStack(){
+    
+    int size = vctrstack.size();
+    int loop = size / 2;
+
+    for(int i = 0; i < loop ; i++){
+        long double temp = vctrstack[size - 1 - i];
+        vctrstack[size - 1 - i] = vctrstack[i];
+        vctrstack[i] = temp;
+    }
+
+    return;
+}
+
+
