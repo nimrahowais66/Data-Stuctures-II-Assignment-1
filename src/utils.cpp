413f2f5c0cd4ee48e0e5e4810aeff1a4350b0c23
unknown <sarfarazsamia26@gmail.com>
2026-02-12 01:11:49 +0500

    Added testing file(donot delete) and attempted utils

--- src/utils.cpp
+++ src/utils.cpp
@@ -7,7 +7,26 @@ std::string removeTrailingZeros(long double number) {
 }
 
 std::vector<std::string> str_split(std::string line) {
-    /*
-    Your implementation goes here
-    */
+    std::vector<std::string> output;
+    int length = line.length();
+    int start = 0;
+    int count = 0;
+
+    for(int i = 0; i < length; i++){
+        if (line[0] == ' '){ start = 1; }
+        else{
+            if (line[i] == ' ' || i == length - 1){
+                output.push_back(line.substr(start, count));
+                start = i + 1;
+                count = 0;
+            } 
+            else{
+                count++;
+            }
+        }
+    }
+
+    return output;
 }
+
+
