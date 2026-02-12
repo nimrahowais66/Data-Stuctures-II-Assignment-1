4a53a00a8bc8388ef33136665c68422e62982379
Nimrah Mustafa <nimrahowais66@gmail.com>
2026-02-12 12:30:17 +0500

    removeTrailingZeros done

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,9 +1,35 @@
 #include "../include/utils.hpp"
 
+#include <iostream>
+
 std::string removeTrailingZeros(long double number) {
-    /*
-    Your implementation goes here
-    */
+    std::string s = std::to_string(number);
+    size_t decimal_pos = s.find('.');
+    bool decimal_found = false;
+    if (decimal_pos != std::string::npos) {
+        decimal_found = true;
+    }
+    while (s[0] == '0' || s[s.length() - 1] == '0') {
+        if (s[0] == '0') {
+            s.erase(0, 1);
+        }
+        if (s[s.length() - 1] == '0' && decimal_found) {
+            s.pop_back();
+        }
+    }
+    if (s.length() == 1 && s[0] == '.') {
+        s = "0";
+    }
+     if (s.length() == 0) {
+        s = "0";
+    }
+     if (s[0] == '.' && s.length() > 1) {
+        s = "0" + s;
+    }
+     if (s[s.length() - 1] == '.' && s.length() > 1) {
+        s.pop_back();
+    }
+    return s;
 }
 
 std::vector<std::string> str_split(std::string line) {
@@ -29,4 +55,14 @@ std::vector<std::string> str_split(std::string line) {
     return output;
 }
 
-
+// testing removeTrailingZeros
+int main() {
+    long double num = 000123.45000180000;
+    std::cout << removeTrailingZeros(123.450000) << std::endl; // Output: "123.45"
+    std::cout << removeTrailingZeros(0.000000) << std::endl;   // Output:
+    std::cout << removeTrailingZeros(100.1000000) << std::endl; // Output: "100"
+    std::cout << removeTrailingZeros(0.000001) << std::endl;   // Output: "0.000001"
+    std::cout << removeTrailingZeros(00001.000000) << std::endl; // Output: "1"
+    std::cout << removeTrailingZeros(num) << std::endl; // Output: "125.5900045"
+    return 0;
+}
