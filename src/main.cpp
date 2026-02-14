faa8c3d9c88d5811bd8cddc44aa91f2ed720fac3
unknown <sarfarazsamia26@gmail.com>
2026-02-14 23:30:42 +0500

    Complete

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
+#include "stackPostScript.cpp"
+#include "utils.cpp"
+#include "PostScriptFileSimplifier.cpp"
+
 int main() { 
+    PostScriptFileSimplifier mySimplifier("../input/file-2.eps");
+    mySimplifier.display_file();
+    mySimplifier.simplify_definitions();
+    std :: cout << std :: endl;
+    mySimplifier.display_file();
+
+    
     return 0; 
 }
